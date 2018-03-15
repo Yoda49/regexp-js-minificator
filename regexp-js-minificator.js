43f4ae5bf162b50d1ee4c1178eebfbc4899f6af0
yoda49 <upv@bk.ru>
2018-03-16 00:38:21 +0300

    fix some bugs

--- regexp-js-minificator.js
+++ regexp-js-minificator.js
@@ -1,14 +1,14 @@
-// Simple, fast, minimal JavaScript minificator
+// Simple, fast, minimal java-script minificator
+// Version: 0.0.1
+// Author: Yoda49
 
 function jsm (text)
 {
 	text = text.replace(/\/\/.*\n/gi,""); // delete comment
 	text = text.replace(/\/\/.*\r/gi,""); // delete comment
-	text = text.replace(/\t/gi,""); // delete tab
 	text = text.replace(/\/\*([\s\S]*)\*\//gi,""); // delete comment /* */
-	text = text.replace(/[\n\r]/gi,"");
-	text = text.replace(/\s*if\s*/gi,"if");
-	text = text.replace(/\s*else\s*/gi,"else");
+	text = text.replace(/if\s*/gi,"if");
+	text = text.replace(/\s*else/gi,"else");
 	text = text.replace(/\s*\(\s*/gi,"(");
 	text = text.replace(/\s*\)\s*/gi,")");
 	text = text.replace(/\s*\{\s*/gi,"{");
@@ -32,6 +32,9 @@ function jsm (text)
 	text = text.replace(/\s*>=\s*/gi,'>=');
 	text = text.replace(/\s*<=\s*/gi,'<=');
 	text = text.replace(/\s*;\s*/gi,';');
+	text = text.replace(/\t/gi,""); // delete tab
+	text = text.replace(/\n/gi,"");
+	text = text.replace(/\r/gi,"");
 	return text;
 }
 
